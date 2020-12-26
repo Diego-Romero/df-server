990401b9b1c63e32920956926f172f652f9ff1b6
diego-romero <dev.diego.romero@gmail.com>
2020-12-26 17:17:20 -0600

    Updated project to use nodemon

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+### Adding env variables
+- Update nodemon.config file with the needed files
+- To test a build, create a .env file and follow the example from .env.example
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-require('dotenv')
-
 interface Config {
     port: string | undefined,
     env: string | undefined
@@ -13,5 +11,7 @@ const config: Config = {
 for (const value of Object.values(config)) {
     if (value === undefined) throw new Error("ERROR: missing to add fields to .env");
 }
+
+console.log(`CONFIG:`, config)
 
 export default config
