1ce50a9a3d1238cf95ed6f108d976642406e8c2d
diego-romero <dev.diego.romero@gmail.com>
2020-12-27 11:51:58 -0600

    fixed error of missing fields in config

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,11 @@
 interface Config {
     port: string | undefined,
     env: string | undefined
-    dbUser: string | undefined
-    dbPassword: string | undefined
-    dbName: string | undefined
 }
 
 const config: Config = {
   port: process.env.PORT,
   env: process.env.NODE_ENV,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbName: process.env.DB_NAME,
 };
 
 console.log(config)
